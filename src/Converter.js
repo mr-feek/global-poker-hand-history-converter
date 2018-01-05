1d3c75334a99c0cd28a5824f346bda06691dbd72
Feek <fpm5022@gmail.com>
2018-01-05 01:09:55 -0500

    these comments are no longer necessary

--- src/Converter.js
+++ src/Converter.js
@@ -1,10 +1,8 @@
 export function convertTitle(hand) {
-    // PokerStars Game #109682170558:  Hold'em No Limit ($0.05/$0.10 USD) - 2014/01/06 7:47:13 ET'
     return `PokerStars Game #${hand.handId}:  Hold'em No Limit ($${hand.smallBlind}/$${hand.bigBlind} USD) - ${hand.timePlayed}`;
 }
 
 export function convertDescription(hand) {
-    // Table 'Klinkenberg Zoom 40-100 bb' 6-max Seat #1 is the button
     return `Table '${hand.tableName} ${hand.minBuyIn}-${hand.maxBuyIn} bb' ${hand.maxSeats}-max Seat #${hand.buttonSeatNumber} is the button`;
 }
 
@@ -60,7 +58,6 @@ export function convertRiverActions(hand) {
 }
 
 export function convertPotInfo(hand) {
-    // Total pot $1.57 | Rake $0.07
     return `Total pot $${hand.totalPot} | Rake $${hand.totalRake}`;
 }
 
